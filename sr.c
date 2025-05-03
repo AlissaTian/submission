5b3d06f5eb256e4001b3cbeb9911845a16f44899
AlissaTian <a1880923@adelaide.edu.au>
2025-05-04 01:52:07 +0800

    Add protocol constants and utility functions. Set SEQSPACE to 14 to ensure it's at least twice the window size as required for SR

--- sr.c
+++ sr.c
@@ -13,3 +13,32 @@
    - Buffering of out-of-order packets at receiver
    - Individual acknowledgment of packets
 *********************************************************************/
+
+#define RTT  16.0       /* round trip time. MUST BE SET TO 16.0 when submitting assignment */
+#define WINDOWSIZE 6    /* the maximum number of buffered unacked packet */
+#define SEQSPACE 14     /* the sequence space for SR must be at least 2*windowsize */
+#define NOTINUSE (-1)   /* used to fill header fields that are not being used */
+#define FALSE 0
+#define TRUE 1
+
+/* generic procedure to compute the checksum of a packet */
+int ComputeChecksum(struct pkt packet)
+{
+  int checksum = 0;
+  int i;
+
+  checksum = packet.seqnum;
+  checksum += packet.acknum;
+  for (i=0; i<20; i++) 
+    checksum += (int)(packet.payload[i]);
+
+  return checksum;
+}
+
+int IsCorrupted(struct pkt packet)
+{
+  if (packet.checksum == ComputeChecksum(packet))
+    return (FALSE);
+  else
+    return (TRUE);
+}
